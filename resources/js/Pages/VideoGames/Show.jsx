963e8a23b2ed572ca9546d4433a43952db9482fc
Waylon Dixon <wayjay4@yahoo.com>
2024-02-02 13:37:51 -0700

    make the single game page responsive

--- resources/js/Pages/VideoGames/Show.jsx
+++ resources/js/Pages/VideoGames/Show.jsx
@@ -4,12 +4,12 @@ export default function show() {
     return (
         <VideoGameLayout>
             <div className="container mx-auto px-4">
-                <div className="game-details border-b border-gray-800 pb-12 flex">
+                <div className="game-details border-b border-gray-800 pb-12 flex flex-col lg:flex-row">
                     <div className="flex-non">
                         <img src="/assets/images/ff7.jpg" alt="cover" />
                     </div>
-                    <div className="ml-12 mr-64">
-                        <h2 className="font-semibold text-4xl">Final Fantasy VII Remake</h2>
+                    <div className="lg:ml-12 lg:mr-64 mt-8 lg:mt-0">
+                        <h2 className="font-semibold text-4xl leading-tight mt-1">Final Fantasy VII Remake</h2>
                         <div className="text-gray-400">
                             <span>Adventure, RPG</span>
                             &middot;
@@ -30,7 +30,7 @@ export default function show() {
                                 </div>
                                 <div className="ml-4 text-xs">Critics <br/> Score</div>
                             </div>
-                            <div className="flex items-center space-x-4">
+                            <div className="flex items-center space-x-4 mt-4 lg:mt-0">
                                 <div className="w-8 h-8 bg-gray-800 rounded-full flex justify-center items-center">
                                     <a href="" className="hover:text-gray-400">
                                         <svg className="w-5 h-5 fill-current" viewBox="0 0 16 17" fill="none"><path d="M8 .266C3.582.266 0 3.952 0 8.5s3.582 8.234 8 8.234 8-3.686 8-8.234S12.418.266 8 .266zm2.655 11.873l-.365.375a.8.8 0 00-.2.355c-.048.188-.087.378-.153.56l-.561 1.556c-.444.1-.903.156-1.376.156v-.91c.055-.418-.246-1.203-.73-1.701-.194-.2-.302-.47-.302-.752v-1.062c0-.387-.203-.742-.531-.93a52.733 52.733 0 00-1.575-.866 4.648 4.648 0 01-1.02-.722l-.027-.024a3.781 3.781 0 01-.582-.689c-.303-.457-.796-1.209-1.116-1.698a6.581 6.581 0 013.33-3.383l.774.399c.343.177.747-.08.747-.475v-.375c.257-.043.52-.07.787-.08l.912.939a.542.542 0 010 .751l-.15.156-.334.343c-.101.104-.101.272 0 .376l.15.155c.102.104.102.272 0 .376l-.257.265a.255.255 0 01-.183.078h-.29a.254.254 0 00-.18.076l-.32.32a.269.269 0 00-.05.31l.502 1.035c.086.176-.039.384-.23.384h-.182a.253.253 0 01-.17-.065l-.299-.268a.51.51 0 00-.501-.102l-1.006.345a.386.386 0 00-.19.144.405.405 0 00.14.587l.357.184c.304.156.639.238.978.238.34 0 .729.906 1.032 1.062h2.153c.274 0 .537.112.73.311l.442.455c.184.19.288.447.288.716a1.584 1.584 0 01-.442 1.095zm2.797-3.033a.775.775 0 01-.457-.331l-.58-.896a.812.812 0 010-.883l.632-.976a.78.78 0 01.298-.27l.419-.216c.436.894.688 1.9.688 2.966 0 .288-.024.57-.06.848l-.94-.242z" /></svg>
@@ -69,7 +69,7 @@ export default function show() {
                 </div> {/*end game details*/}
                 <div className="images-container border-b border-gray-800 pb-12 mt-8">
                     <h2 className="text-blue-500 uppercase tracking-wide font-semibold">Screenshots</h2>
-                    <div className="grid grid-cols-3 gap-12 mt-8">
+                    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-8">
                         <div>
                             <a href="#">
                                 <img src="/assets/images/screenshot1.jpg" className="hover:opacity-75 transition ease-in-out duration-150" />
